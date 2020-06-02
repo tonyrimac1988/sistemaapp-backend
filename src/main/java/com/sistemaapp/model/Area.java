b76223fb8102e23ac44775ebabc2be05987a47a5
tonyrimac1988 <tony.rimac88@gmail.com>
2020-06-02 17:12:03 -0500

    se agrego un espacio

--- src/main/java/com/sistemaapp/model/Area.java
+++ src/main/java/com/sistemaapp/model/Area.java
@@ -18,7 +18,7 @@ public class Area {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer idArea;
+	private Integer idArea; 
 	
 	@Column(name = "nombre", nullable = false, length = 100)
 	private String nombre;
